3fe2408fc5d322405629ec93e27a1deb9329f249
Roland Robe <roland.robe@gmail.com>
2020-12-28 11:59:30 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app=express();
+app.get('/', (req,res)=>{
+    res.status(200).send('Hallo');
+});
+app.listen(80,()=>{
+    console.log('Listen on Port 80')
+});
+
