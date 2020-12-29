4a92306a9a7158e0fbb725473666c1a841148ae1
Roland Robe <roland.robe@gmail.com>
2020-12-29 11:30:01 +0100

    Added HalloVelo Endpoint

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port=process.env.PORT || 80;
 app.get('/', (req,res)=>{
     res.status(200).send('Hallo');
 });
+
+
+app.get('/hallo', (req,res)=>{
+    res.send('Velo');
+});
+
 app.listen(port,()=>{
     console.log('Listen on Port 80');
 });
